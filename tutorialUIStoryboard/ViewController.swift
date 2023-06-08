5a3261c82545b84b70c627055f6fade9a1396a8d
Ykylas <albertov.sasha@mail.ru>
2023-06-08 16:55:54 +0600

    Initial Commit

---
+++ tutorialUIStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tutorialUIStoryboard
+//
+//  Created by Ykylas Nurkhan on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
